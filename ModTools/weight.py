ec093c08103ea9f2391132e4f0c44edd626dc13a
Hollow <1210923429@qq.com>
2026-07-14 10:24:04 +0800

    引用强制应用骨架姿势

--- ModTools/weight.py
+++ ModTools/weight.py
@@ -1,4 +1,5 @@
 from ..BoneTools.boneHumanoid import OP_MoveHumanoidBonesToCollection
+from ..BoneTools.boneOperators import OP_ApplyRestPose
 
 
 def drawWeightPanel(layout, context):
@@ -6,4 +7,9 @@ def drawWeightPanel(layout, context):
     column.operator(
         OP_MoveHumanoidBonesToCollection.bl_idname,
         icon='GROUP_BONE',
+    )
+    column.operator(
+        OP_ApplyRestPose.bl_idname,
+        text="强制应用姿态与Mesh",
+        icon='ARMATURE_DATA',
     )
